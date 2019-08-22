2ba2aba9c7dc785f6b79e34585570d79d2a0411d
JorgeiMac <jaguay@yahoo.com>
2019-08-21 21:18:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-######
-# Lee archivos XML y los datos los pasa a un archivo de lectura facil
-# Version 1
-# Autor: jorge Aguayo
-# Fecha ultima revision: 2019/08/02
-######
+'''
+ Lee archivos XML y los datos los pasa a un archivo de lectura facil
+ Version 1
+ Autor: jorge Aguayo
+ Fecha ultima revision: 2019/08/02
+'''
 
 # Esta libreria sirve para leer archvos
 import glob
@@ -15,13 +15,13 @@ import xml.etree.ElementTree as ET
 print("Primer prueba para leer xml")
 
 # lee los archivos del folder especificado
-#archivosxml = glob.glob("/Users/jorgeaguayo/Documents/Desarrollo/ConciliacionCFDI/conciliacion-cfdi/_xmlFiles/*.xml")
+# archivosxml = glob.glob("/Users/jorgeaguayo/Documents/Desarrollo/ConciliacionCFDI/conciliacion-cfdi/_xmlFiles/*.xml")
 archivosxml = glob.glob("_xmlFiles/*.xml")
 
 
 # for archivo in archivosxml:
-    #aqui debe de leer cada arcxhivo xml que se le va dando y lo sube a un archivo de texto con el extracto
- #  print(archivo)
+#    aqui debe de leer cada arcxhivo xml que se le va dando y lo sube a un archivo de texto con el extracto
+#  print(archivo)
 
 
 tree = ET.parse('_xmlFiles/ade02b7b-e629-4dbb-a0e6-e17d9de74980@1000000000XX0.xml')
@@ -34,7 +34,6 @@ for elem in root:
         print(subelem.attrib)
 
 
-
 # one specific item's data
 print('\nDirecto:')
 print(root[0].attrib)
@@ -43,3 +42,4 @@ print('\nDirecto2:')
 
 for emisor in root.iter('cfdi:Emisor'):
     print(emisor.attrib)
+
